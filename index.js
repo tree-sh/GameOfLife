13f0505100252a6cd33b9b680d883b5850324e8b
C <mistertomnook@protonmail.com>
2023-02-07 23:35:38 +0800

    create first grid

---
+++ index.js
@@ -0,0 +1,25 @@
+var lastClicked;
+var grid = clickableGrid(100,50,function(el,row,col,i){
+    el.className === "clicked" ? el.className = "" : el.className = "clicked";
+});
+
+document.body.appendChild(grid);
+     
+function clickableGrid( rows, cols, callback ){
+    var i=0;
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    for (var r=0;r<rows;++r){
+        var tr = grid.appendChild(document.createElement('tr'));
+        for (var c=0;c<cols;++c){
+            var cell = tr.appendChild(document.createElement('td'));
+            cell.style.padding = "25px";
+            cell.addEventListener('click',(function(el,r,c,i){
+                return function(){
+                    callback(el,r,c,i);
+                }
+            })(cell,r,c,i),false);
+        }
+    }
+    return grid;
+}
